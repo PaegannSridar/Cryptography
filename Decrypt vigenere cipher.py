b1d9be4d1856540c2d4d4a0a44f13b735361ca15
PaegannSridar <paegann.sridar@gmail.com>
2024-11-07 18:08:21 +0000

    Updating programs

--- Decrypt vigenere cipher.py
+++ Decrypt vigenere cipher.py
@@ -1,40 +1,43 @@
-from math import gcd
+from collections import Counter
 
-def mod_inverse(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return None
-
-def affine_decrypt(ciphertext, a, b, m=26):
-    plaintext = ''
-    a_inv = mod_inverse(a, m)
-    if a_inv is None:
-        return None
-    for char in ciphertext:
-        if char.isalpha():
-            x = ord(char.upper()) - ord('A')
-            plain_char = (a_inv * (x - b)) % m
-            plaintext += chr(plain_char + ord('A'))
+def caesar_decrypt(text, shift):
+    decrypted = []
+    for char in text:
+        if 'A' <= char <= 'Z':
+            decrypted.append(chr((ord(char) - ord('A') - shift) % 26 + ord('A')))
+        elif 'a' <= char <= 'z':
+            decrypted.append(chr((ord(char) - ord('a') - shift) % 26 + ord('a')))
         else:
-            plaintext += char
-    return plaintext
+            decrypted.append(char)
+    return ''.join(decrypted)
 
-def affine_crack(ciphertext, m=26):
-    possible_plaintexts = []
-    for a in range(1, m):
-        if gcd(a, m) == 1:
-            for b in range(m):
-                plaintext = affine_decrypt(ciphertext, a, b, m)
-                if plaintext:
-                    possible_plaintexts.append((a, b, plaintext))
-    return possible_plaintexts
+def vigenere_decrypt(ciphertext, key_length):
+    def guess_caesar_shift(segment):
+        frequency = Counter(segment)
+        most_common_char = frequency.most_common(1)[0][0]
+        shift = (ord(most_common_char) - ord('E')) % 26
+        return shift
+
+    segments = ['' for _ in range(key_length)]
+    for i, char in enumerate(ciphertext):
+        segments[i % key_length] += char
+
+    all_shifts = []
+    for segment in segments:
+        possible_shifts = []
+        for shift in range(26):
+            shifted_segment = caesar_decrypt(segment, shift)
+            possible_shifts.append((shift, shifted_segment))
+        all_shifts.append(possible_shifts)
+
+    return all_shifts
 
 ciphertext = "MZDFP OCFFO YFQZY ZMXDE CTCWZ HOWCS OCYTX DSPCD WOEUC UUCNO PZDEW OKFZI ODEOC EFPZD EWOKE GDDOK OYNWC YTXKT OCDSZ GYFOE EXDUC UUCNO QPCHO SZXAW OFOTX KQYHO EFQNC FQZYE CFFPO QXAOD QCWCD XESGE FZXEI CDOPZ GEOCY TIQWW DOFGD YMZDF PIQFP UGFVY ZIQYN RGEFP ZIWZY NFPCF RZGDY OKXCK FCVOC YTPZI DOXCD VCUWK EIQMF KZGDD ZKCWX CQWSC YUOQF PZGNP FQFUO EFFZE OYTKZ GXKDO AZDFU KWOFF ODXKY OIMDQ OYTEQ YWQHO DAZZW IODOD OCWPO WAMGW CYTAD ZHQTO TXOIQ FPFPO EPQAA QYNAC AODEC WFPZG NPQCX YZFEG DOPZI XGSPC EEQEF CYSOF POKIQ WWADZ HQTOC WWQSC YEOOQ EFPCF FPOSD CFOEC WWWOM FFPOG YQFOT EFCFO EFZNO FPODC YTOCS PIOQN POTFP OECXO EGNNO EFQYN FPCFY ZFPQY NPCTU OOYFC VOYQM QYTQF XQNPF KGYWQ VOWKF PCFCY KZYOQ YFODO EFOTQ YEFOC WQYNF POSZY FOYFE ZMCEQ YNWOS DCFOI ZGWTF CVOFP OFDZG UWOFZ DOAWC SOFPO XIQFP CSCDO MGWWK SCWQU DCFOT IOQNP FZMDG UUWOZ DZFPO DYZYE OYEOC YTQYC YKSCE OFPOE OCWPC TUOOY DOAWC SOTFP CFEOO XEWQV OCYCI MGWWZ FZMFD ZGUWO MZDCM OIDQM WOECY TUZJO EZMCX XGYQF QZYIQ FPFPO SDCFO EQYEF ZDCNO FPOEO CWEPC HOUOO YDOXZ HOTAD OEGXC UWKFZ CWWZI KZGDS GEFZX EZMMQ SQCWE FZQYE AOSFF POXCY TFPCF NCHOX OCYZA AZDFG YQFKF ZOJCX QYOFP OSZYF OYFEQ FFZZV XOCWZ YNFQX OFZIZ DVFPD ZGNPF POXCW WUGFQ FEFPO TCDYO TOEFF PQYNF PODOQ EYZFP QYNXQ EEQYN CYTQX OCYYZ FPQYN YZFOH OYCEQ YNWOU GWWOF CYTIO VYZIF PCFCF WOCEF ZYOIC EWOMF ZYFPO MWZZD ZMFPO ICDOP ZGEOQ YODQY XKMDQ OYTEF OWWXO FPCFC PQYTG NOYFW OXCYP CTUOO YEOOY QYFPO HQSQY QFKZM FPOEP OTQYF POACE FYQNP FUGFQ CXXQY TOTFZ FPQYV FPCFQ ERGEF ESGFF WOUGF FMZWV EQYOD QYFCW VWQVO FPCFC UZGFE FDCYN ODECW WFPOF QXOCY TIQFP YZFPQ YNFZS ZYYOS FPQXF ZFPOS CDNZC YTYZI CKFZF DCSVP QXTZI YQFPQ YVIOS CYQNY ZDOQM YZFMZ DNOFF PONZE EQAQP ZAOFZ UOIQF PKZGC NCQYQ YFPDO OTCKE CYTWZ ZVMZD ICDTF ZTQES GEEQY NFPQE XKEFO DKIQF PKZGF POYXQ EEVCF OICDY O"
 
-results = affine_crack(ciphertext)
+key_length = 6
+decrypted_shifts = vigenere_decrypt(ciphertext, key_length)
 
-for a, b, plaintext in results:
-    print(f"a={a}, b={b}: {plaintext}")
-
-
+for i, shifts in enumerate(decrypted_shifts):
+    print(f"Segment {i+1}:")
+    for shift, text in shifts:
+        print(f"Shift {shift}: {text}")
